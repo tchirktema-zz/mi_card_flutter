80d0e075c51c99c988cc11ecaf8e5d794768a5e9
Josue Tchirktema <tchirktemajosue@gmail.com>
2019-09-05 01:57:33 +0000

    design

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,85 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepOrange,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundColor: Colors.white,
+                    backgroundImage: AssetImage('assets/images/joshua.jpg'),
+                  ),
+                  Text(
+                    'Tchirktema josue',
+                    style: TextStyle(
+                      fontSize: 35.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Pacifico'
+                    ),
+                  ),
+                  Text(
+                    'FLUTTER DEVELOPPER',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.teal.shade100,
+                      fontSize: 20.0,
+                      letterSpacing: 2,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                    width: 150.0,
+                    child: Divider(
+                      color: Colors.white,
+
+                    ),
+                  ),
+                  Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                    child: ListTile(
+                        leading : Icon(
+                          Icons.phone,
+                          color: Colors.teal.shade900,
+                        ),
+                        title : Text(
+                          '+225 00 00 00 00 00',
+                          style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontFamily: 'Source Sans Pro',
+                              fontSize: 20.0
+                          ),
+                        )
+
+                    ),
+                  ),
+                  Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                    child: ListTile(
+                        leading : Icon(
+                          Icons.email,
+                          color: Colors.teal.shade900,
+                        ),
+
+                        title: Text(
+                          'tchirktemajosue@gmail.com',
+                          style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontFamily: 'Source Sans Pro',
+                              fontSize: 20.0
+                          ),
+                        )
+
+                    ),
+                  )
+                ],
+              ),
+            )
+        ),
       ),
     );
   }
